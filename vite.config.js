c106289211809aa35e15ccd2f7f73498c3d44d0b
MarianoConesa <1784440@alu.murciaeduca.es>
2024-11-04 14:35:39 +0100

    cambio ip vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], server: {port: '3000', host: '192.168.18.21'}
+  plugins: [react()], server: {port: '3000', host: '192.168.18.79'}
 })
